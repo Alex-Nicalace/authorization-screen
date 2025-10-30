1c395b36b63d87332365c0dd61dfb16443f95ef4
Alex-Nicalace <anicalace@gmail.com>
2025-10-30 16:02:19 +0200

    feat: add status handling with icons for Input component

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,26 +1,63 @@
 import clsx from "clsx";
 import type { InputHTMLAttributes } from "react";
+import SuccessIcon from "../../assets/icons/check-circle-filled.svg";
+import ErrorIcon from "../../assets/icons/close-circle-filled.svg";
+import WarningIcon from "../../assets/icons/exclamation-circle-filled.svg";
 import type { Size } from "../../types";
+
+type Status = "default" | "error" | "warning" | "success";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   startAdornment?: React.ReactNode;
   dimension?: Size;
+  status?: Status;
 }
 
 type Component = "wrap" | "input" | "icon";
 
 const STYLES_BASE: Record<Component, string> = {
   wrap: clsx(
-    "flex items-center gap-1 rounded-lg border border-gray-light transition-colors",
-    "hover:not-focus-within:not-[:has(input:disabled)]:border-primary-light",
-    "focus-within:border-primary-dark",
-    "has-[input:disabled]:bg-black-opacity-4",
+    "flex items-center gap-1 rounded-lg border transition-colors",
+    "has-[input:disabled]:border-gray-light has-[input:disabled]:bg-black-opacity-4",
   ),
   input: clsx(
     "flex-1 text-black/88 outline-0 placeholder:text-black-opacity-25",
     "disabled:cursor-not-allowed",
   ),
-  icon: clsx("text-black-opacity-45 [&>svg]:h-full [&>svg]:w-full"),
+  icon: clsx("transition-colors [&>svg]:h-full [&>svg]:w-full"),
+};
+
+const STYLES_STATUS: Record<Status, Partial<Record<Component, string>>> = {
+  default: {
+    wrap: clsx(
+      "border-gray-light",
+      "hover:not-focus-within:not-[:has(input:disabled)]:border-primary-light",
+      "focus-within:border-primary-dark focus-within:shadow-[0_0_0_2px_rgba(5,145,255,0.1)]",
+    ),
+    icon: clsx("text-black-opacity-45"),
+  },
+  error: {
+    wrap: clsx(
+      "border-error text-error",
+      "hover:not-focus-within:not-[:has(input:disabled)]:border-error-light hover:not-focus-within:not-[:has(input:disabled)]:[&>input+*]:text-error-light",
+      "focus-within:shadow-[0_0_0_2px_rgba(255,38,6,0.06)]",
+    ),
+  },
+  warning: {
+    wrap: clsx(
+      "border-warning text-warning",
+      "hover:not-focus-within:not-[:has(input:disabled)]:border-warning-light hover:not-focus-within:not-[:has(input:disabled)]:[&>input+*]:text-warning-light",
+      "focus-within:shadow-[0_0_0_2px_rgba(255,215,5,0.1)]",
+    ),
+  },
+  success: {
+    wrap: clsx(
+      "border-gray-light text-success",
+      "hover:not-focus-within:not-[:has(input:disabled)]:border-primary-light",
+      "focus-within:border-primary-dark focus-within:shadow-[0_0_0_2px_rgba(5,145,255,0.1)]",
+    ),
+    icon: clsx(""),
+  },
 };
 
 const STYLES_SIZE: Record<Size, Record<Component, string>> = {
@@ -45,15 +82,25 @@ export default function Input({
   className,
   startAdornment,
   dimension = "default",
+  status = "default",
   ...props
 }: InputProps) {
   return (
     <div
-      className={clsx(STYLES_BASE.wrap, STYLES_SIZE[dimension].wrap, className)}
+      className={clsx(
+        STYLES_BASE.wrap,
+        STYLES_SIZE[dimension].wrap,
+        STYLES_STATUS[status].wrap,
+        className,
+      )}
     >
       {startAdornment && (
         <div
-          className={clsx(STYLES_BASE.icon, STYLES_SIZE[dimension].icon)}
+          className={clsx(
+            STYLES_BASE.icon,
+            STYLES_SIZE[dimension].icon,
+            STYLES_STATUS[status].icon,
+          )}
           aria-hidden="true"
         >
           {startAdornment}
@@ -63,6 +110,16 @@ export default function Input({
         {...props}
         className={clsx(STYLES_BASE.input, STYLES_SIZE[dimension].input)}
       />
+      {status !== "default" && (
+        <div
+          className={clsx(STYLES_BASE.icon, STYLES_SIZE[dimension].icon)}
+          aria-hidden="true"
+        >
+          {status === "error" && <ErrorIcon />}
+          {status === "warning" && <WarningIcon />}
+          {status === "success" && <SuccessIcon />}
+        </div>
+      )}
     </div>
   );
 }
