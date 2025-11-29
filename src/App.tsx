e4d2059f08692f326d697fb979e48e94458d9a68
Alex-Nicalace <anicalace@gmail.com>
2025-11-29 13:20:28 +0200

    feat: usage AuthPage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import "./App.css";
+import { AuthPage } from "./pages/AuthPage";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-center text-xl font-semibold text-yellow-500">
-        Vite + React
-      </h1>
-    </div>
+    <>
+      <AuthPage />
+    </>
   );
 }
 
