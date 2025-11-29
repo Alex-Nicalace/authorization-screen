11748f2d6ec2cb90a1077109b5c44322ea65a764
Alex-Nicalace <anicalace@gmail.com>
2025-11-29 13:54:27 +0200

    feat: implement step navigation in AuthPage

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import LoginForm from "./LoginForm";
 import OtpForm from "./OtpForm";
 
+type Step = "login" | "otp";
+
 // interface AuthPageProps {}
 export default function AuthPage(/*{ }: AuthPageProps*/) {
+  const [step, setStep] = useState<Step>("login");
+
+  const handleSuccessLoginForm = () => {
+    console.log("otp");
+
+    setStep("otp");
+  };
+
   return (
     <main className="page-container flex min-h-full items-center justify-center">
-      <LoginForm className="max-w-110" />
-      <OtpForm className="max-w-110" />
+      {step === "login" && (
+        <LoginForm className="max-w-110" onSuccess={handleSuccessLoginForm} />
+      )}
+      {step === "otp" && (
+        <OtpForm className="max-w-110" onClickBack={() => setStep("login")} />
+      )}
     </main>
   );
 }
