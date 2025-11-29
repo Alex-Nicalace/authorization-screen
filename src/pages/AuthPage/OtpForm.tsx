8d37eece65c72b74ed3b0c2dfb02d9f1b4c2573c
Alex-Nicalace <anicalace@gmail.com>
2025-11-29 13:53:19 +0200

    feat: OtpForm component add onClickBack prop

--- src/pages/AuthPage/OtpForm.tsx
+++ src/pages/AuthPage/OtpForm.tsx
@@ -1,20 +1,33 @@
 import clsx from "clsx";
+import ArrowLeftIcon from "../../assets/icons/arrow-left-outlined.svg";
 import Logo from "../../components/Logo";
-import Title from "../../components/ui/Title";
-import OtpInput from "../../components/ui/OtpInput";
 import Button from "../../components/ui/Button";
+import OtpInput from "../../components/ui/OtpInput";
+import Title from "../../components/ui/Title";
 
-// type OtpFormProps = { }
+interface OtpFormProps extends Omit<React.ComponentProps<"form">, "onSubmit"> {
+  onSuccess?: () => void;
+  onClickBack?: () => void;
+}
+
 export default function OtpForm({
   className,
+  onClickBack,
   ...props
-}: React.ComponentProps<"form">) {
+}: OtpFormProps) {
   return (
     <form
       className={clsx("flex flex-col gap-6 rounded-md bg-white p-8", className)}
       {...props}
     >
-      <div className="flex flex-col items-center gap-1 text-center text-pretty">
+      <div className="relative flex flex-col items-center gap-1 text-center text-pretty">
+        <Button
+          className="absolute top-0 left-0"
+          onClick={onClickBack}
+          mode="text"
+        >
+          <ArrowLeftIcon />
+        </Button>
         <Logo className="py-5" />
         <Title level={3}>Two-Factor Authentication</Title>
         <p>Enter the 6-digit code from the Google Authenticator app</p>
