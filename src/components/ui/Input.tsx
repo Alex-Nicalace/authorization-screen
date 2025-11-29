fd72f081435a54336740858fcec522673d890e84
Alex-Nicalace <anicalace@gmail.com>
2025-11-29 11:57:42 +0200

    feat: add helperText prop to Input

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -9,11 +9,12 @@ interface InputProps extends React.ComponentPropsWithRef<"input"> {
   dimension?: Size;
   status?: Status;
   isStatusIcon?: boolean;
+  helperText?: string;
 }
 
-type Component = "wrap" | "input" | "icon";
+type Component = "wrap" | "input" | "icon" | "root" | "helperText";
 
-const STYLES_BASE: Record<Component, string> = {
+const STYLES_BASE: Partial<Record<Component, string>> = {
   wrap: clsx(
     "flex items-center gap-1 rounded-lg border transition-colors",
     "has-[input:disabled]:border-gray-light has-[input:disabled]:bg-black-opacity-4",
@@ -23,10 +24,12 @@ const STYLES_BASE: Record<Component, string> = {
     "disabled:cursor-not-allowed",
   ),
   icon: clsx("transition-colors [&>svg]:h-full [&>svg]:w-full"),
+  helperText: clsx("mx-4 mt-0.5 text-xs"),
 };
 
 const STYLES_STATUS: Record<Status, Partial<Record<Component, string>>> = {
   default: {
+    root: clsx(""),
     wrap: clsx(
       "border-gray-light",
       "hover:not-focus-within:not-[:has(input:disabled)]:border-primary-light",
@@ -35,15 +38,17 @@ const STYLES_STATUS: Record<Status, Partial<Record<Component, string>>> = {
     icon: clsx("text-black-opacity-45"),
   },
   error: {
+    root: clsx("text-error"),
     wrap: clsx(
-      "border-error text-error",
+      "border-error",
       "hover:not-focus-within:not-[:has(input:disabled)]:border-error-light hover:not-focus-within:not-[:has(input:disabled)]:[&>input+*]:text-error-light",
       "focus-within:shadow-[0_0_0_2px_rgba(255,38,6,0.06)]",
     ),
   },
   warning: {
+    root: clsx("text-warning"),
     wrap: clsx(
-      "border-warning text-warning",
+      "border-warning",
       "hover:not-focus-within:not-[:has(input:disabled)]:border-warning-light hover:not-focus-within:not-[:has(input:disabled)]:[&>input+*]:text-warning-light",
       "focus-within:shadow-[0_0_0_2px_rgba(255,215,5,0.1)]",
     ),
@@ -58,7 +63,7 @@ const STYLES_STATUS: Record<Status, Partial<Record<Component, string>>> = {
   },
 };
 
-const STYLES_SIZE: Record<Size, Record<Component, string>> = {
+const STYLES_SIZE: Record<Size, Partial<Record<Component, string>>> = {
   small: {
     wrap: clsx("px-1.75"),
     input: clsx("text-sm/6"),
@@ -86,43 +91,59 @@ export default function Input({
   dimension = "default",
   status = "default",
   isStatusIcon = true,
+  helperText,
   ...props
 }: InputProps) {
   return (
     <div
-      className={clsx(
-        STYLES_BASE.wrap,
-        STYLES_SIZE[dimension].wrap,
-        STYLES_STATUS[status].wrap,
-        className,
-      )}
+      className={clsx(STYLES_STATUS[status].root)}
       onClick={handleClickWrapper}
     >
-      {startAdornment && (
-        <div
+      <div
+        className={clsx(
+          STYLES_BASE.wrap,
+          STYLES_SIZE[dimension].wrap,
+          STYLES_STATUS[status].wrap,
+          className,
+        )}
+      >
+        {startAdornment && (
+          <div
+            className={clsx(
+              STYLES_BASE.icon,
+              STYLES_SIZE[dimension].icon,
+              STYLES_STATUS[status].icon,
+            )}
+            aria-hidden="true"
+          >
+            {startAdornment}
+          </div>
+        )}
+        <input
+          {...props}
+          className={clsx(STYLES_BASE.input, STYLES_SIZE[dimension].input)}
+        />
+        {status !== "default" && isStatusIcon && (
+          <div
+            className={clsx(STYLES_BASE.icon, STYLES_SIZE[dimension].icon)}
+            aria-hidden="true"
+          >
+            {status === "error" && <ErrorIcon />}
+            {status === "warning" && <WarningIcon />}
+            {status === "success" && <SuccessIcon />}
+          </div>
+        )}
+      </div>
+      {helperText !== undefined && (
+        <p
           className={clsx(
-            STYLES_BASE.icon,
-            STYLES_SIZE[dimension].icon,
-            STYLES_STATUS[status].icon,
+            STYLES_BASE.helperText,
+            STYLES_SIZE[dimension].helperText,
+            STYLES_STATUS[status].helperText,
           )}
-          aria-hidden="true"
         >
-          {startAdornment}
-        </div>
-      )}
-      <input
-        {...props}
-        className={clsx(STYLES_BASE.input, STYLES_SIZE[dimension].input)}
-      />
-      {status !== "default" && isStatusIcon && (
-        <div
-          className={clsx(STYLES_BASE.icon, STYLES_SIZE[dimension].icon)}
-          aria-hidden="true"
-        >
-          {status === "error" && <ErrorIcon />}
-          {status === "warning" && <WarningIcon />}
-          {status === "success" && <SuccessIcon />}
-        </div>
+          {helperText}
+        </p>
       )}
     </div>
   );
