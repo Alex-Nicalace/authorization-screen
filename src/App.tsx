78f569478b785e1dc41c2828ae49a45cc1d3ab61
Alex-Nicalace <anicalace@gmail.com>
2025-11-30 13:50:34 +0200

    feat: usage QueryClientProvider and add ReactQueryDevtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AuthPage } from "./pages/AuthPage";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <AuthPage />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
