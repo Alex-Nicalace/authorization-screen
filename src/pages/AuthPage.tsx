7a828292599b1ec128bacb0d78979b5ba599a0c6
Alex-Nicalace <anicalace@gmail.com>
2025-12-03 15:21:25 +0200

    feat: usage OTP token

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,15 +1,25 @@
 import { useState } from "react";
 import { LoginForm, OtpForm } from "../features/authentication";
+import type { OtpToken } from "../features/authentication/OtpForm";
 
 type Step = "login" | "otp";
 
 // interface AuthPageProps {}
 export default function AuthPage(/*{ }: AuthPageProps*/) {
   const [step, setStep] = useState<Step>("login");
+  const [otpToken, setOtpToken] = useState<OtpToken>({
+    token: "",
+    expiresAt: 0,
+  });
 
-  const handleSuccessLoginForm = () => {
+  function handleSuccessLoginForm(otpToken: OtpToken) {
     setStep("otp");
-  };
+    setOtpToken(otpToken);
+  }
+
+  function handleSuccessOtpForm() {
+    console.log("logic continue");
+  }
 
   return (
     <main className="page-container flex min-h-full items-center justify-center">
@@ -17,7 +27,12 @@ export default function AuthPage(/*{ }: AuthPageProps*/) {
         <LoginForm className="max-w-110" onSuccess={handleSuccessLoginForm} />
       )}
       {step === "otp" && (
-        <OtpForm className="max-w-110" onClickBack={() => setStep("login")} />
+        <OtpForm
+          otpToken={otpToken}
+          className="max-w-110"
+          onClickBack={() => setStep("login")}
+          onSuccess={handleSuccessOtpForm}
+        />
       )}
     </main>
   );
